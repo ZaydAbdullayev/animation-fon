3bc9658826cc8dbad9dc6c8f3157d6ffb478acbc
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2023-10-26 02:14:53 -0700

    new section and animation

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,16 +4,57 @@ import { BiArrowToRight } from "react-icons/bi";
 import { images } from "./imgs";
 import { Corusel } from "../../components/corusel/corusel";
 
+import img from "../../assets/sticker.png";
+import { BsInstagram, BsLinkedin, BsTelegram } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+
 export const Home = () => {
   return (
     <div className="home">
       <div className="my_info">
-        <h1 className="text_3d">
-          <span>Hello I'm Abdullayev Abdulbosit</span>
-        </h1>
-        <h1 className="text_3d">
-          <span>I'm FullStack Developer</span>
-        </h1>
+        <div className="sosyal_media">
+          <a
+            href="https://instagram.com/zayyad_07"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="instagram"
+          >
+            <BsInstagram />
+          </a>
+          <a
+            href="https://www.facebook.com/Zayd.Huseyn.5"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="facebook"
+          >
+            <BsFacebook />
+          </a>
+          <a
+            href="https://t.me/zayyad_07"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="telegram"
+          >
+            <BsTelegram />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/abdullayev-abdulbosit-1b4b7b1b9/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkedin"
+          >
+            <BsLinkedin />
+          </a>
+        </div>
+        <div className="paragraf">
+          <h1 className="text_3d">
+            <span>Hello I'm Abdullayev Abdulbosit</span>
+          </h1>
+          <h1 className="text_3d">
+            <span>I'm FullStack Developer</span>
+          </h1>
+        </div>
+        <img src={img} alt="my_photo" />
       </div>
       <div className="skills c3d">
         <h1 className="text_3d">
